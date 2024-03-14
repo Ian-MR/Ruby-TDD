c8451743cca3e79e14312041d6c04bb317f2c979
Ian Rodrigues <ianmrdrigues@gmail.com>
2024-03-14 17:10:26 -0300

    falha comparando franc com dollar

--- test.rb
+++ test.rb
@@ -2,18 +2,21 @@ require 'minitest/autorun'
 require_relative 'money'
 
 class Test < Minitest::Test
-    def test_franc_multiplication
-        five = Franc.new(5)
-        assert_equal(Franc.new(10), five.times(2)) 
-        assert_equal(Franc.new(15), five.times(3))
-    end
+    #def test_franc_multiplication
+    #    five = Franc.new(5)
+    #    assert_equal(Franc.new(10), five.times(2)) 
+    #    assert_equal(Franc.new(15), five.times(3))
+    #end
     #def test_multiplication
     #    five = Dollar.new(5)
     #    assert_equal(Dollar.new(10), five.times(2)) 
     #    assert_equal(Dollar.new(15), five.times(3))
     #end
-    #def test_equality
-    #    assert(Dollar.new(5).equals(Dollar.new(5)))
-    #    assert(!Dollar.new(5).equals(Dollar.new(6)))
-    #end
+    def test_equality
+        assert(Dollar.new(5) == Dollar.new(5))
+        assert(!(Dollar.new(5) == Dollar.new(6)))
+        assert(Franc.new(5) == Franc.new(5))
+        assert(!(Franc.new(5) == Franc.new(6)))
+        assert(!(Franc.new(5) == Dollar.new(5)))
+    end
 end
